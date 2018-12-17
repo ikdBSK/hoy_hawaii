8251eab2536e43a69ec796ea12722466eb478ac2
itoken66 <ito.k.bg@m.titech.ac.jp>
2018-12-17 13:32:57 +0900

    アカウント構造,ログイン機能実装.Ajaxでアカウントを新規登録する機能をサーバー側のみ実装

---
+++ app/controllers/DataController.java
@@ -0,0 +1,180 @@
+package controllers;
+
+import play.libs.Json;
+import play.mvc.*;
+
+import views.html.*;
+import data.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class DataController extends Controller {
+    final String default_password = "0000";
+
+    Account admin = new Account("admin", default_password, null, null, null);//管理者アカウント
+    List<Student> students = new ArrayList<Student>();
+    List<Teacher> teachers = new ArrayList<Teacher>();
+
+    //idが一致する生徒を検索
+    public Student get_student(String id) {
+        for(Student s : students){
+            if(s.get_id().equals(id)){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    //idが一致する生徒を検索
+    public Teacher get_teacher(String id) {
+        for(Teacher t : teachers){
+            if(t.get_id().equals(id)){
+                return t;
+            }
+        }
+        return null;
+    }
+
+    //ログインするアカウントIDをsessionに保持し、そのsessionIdをクライアントのCookieに保持して紐付ける
+    //必要ないかも？
+    public void connect_session(String id) {
+        final String sessionId = UUID.randomUUID().toString();
+        session(sessionId, id);
+        response().setCookie(
+                Http.Cookie.builder("sessId", sessionId)
+                        .build()
+        );
+    }
+
+    //sessionとCookie削除
+    public void disconnect_session() {
+        final Http.Cookie sessionId = request().cookie("sessId");
+        if(sessionId != null) {
+            session().remove(sessionId.value());
+            response().discardCookie("sessId");
+        }
+    }
+
+
+
+
+    /**
+     * @return ログインページ
+     */
+    public Result index() {
+        return ok(index.render());
+    }
+
+
+    /**
+     * @return JSON形式の生徒リスト
+     */
+    public Result student_list() {
+        return ok(Json.toJson(students));
+    }
+
+
+    /**
+     * @return JSON形式の教師リスト
+     */
+    public Result teacher_list() {
+        return ok(Json.toJson(teachers));
+    }
+
+
+    /**
+     * idとpassword受け取って
+     * アカウントのタイプに対応するページ返す
+     * @return ログイン後のトップページ
+     */
+    public Result login() {
+        //リクエストbodyからidとpasswordを受け取る
+        final Map<String, String[]> request = request().body().asFormUrlEncoded();
+        final String id = request.get("id")[0];
+        final String password = request.get("password")[0];
+
+        //アカウント検索を楽にするため、生徒のidはSで始まり教師のidはTで始まることを想定
+        if(id.startsWith("S")){
+            //idが一致するアカウント取得
+            final Student student = get_student(id);
+            //idかpasswordが一致しなければエラーメッセージをつけてリダイレクト
+            if(student == null || !student.check_password(password)){
+                flash("errormsg", "IDかpasswordが間違っています");
+                return redirect("/");
+            }
+            connect_session(id);
+            //生徒情報を引数として付加した生徒用のトップページを返す
+            return ok(student_top.render(student));
+        }
+
+        if(id.startsWith("T")){
+            final Teacher teacher = get_teacher(id);
+            if(teacher == null || !teacher.check_password(password)){
+                flash("errormsg", "IDかpasswordが間違っています");
+                return redirect("/");
+            }
+            connect_session(id);
+            return ok(teacher_top.render(teacher));
+        }
+
+        //管理者アカウントと一致しなければエラーメッセージをつけてリダイレクト
+        if(!admin.get_id().equals(id) || !admin.check_password(password)){
+            flash("errormsg", "IDかpasswordが間違っています");
+            return redirect("/");
+        }
+        connect_session(id);
+        //debug用にstudentとteacher登録
+        Student s1 = new Student("S0001", default_password, "生徒1", Account.SexTag.male, "A県B市1-1-1");
+        students.add(s1);
+        Teacher t1 = new Teacher("T0001", default_password, "教師1", Account.SexTag.female, "C県D市1-1-1");
+        teachers.add(t1);
+        return ok(admin_top.render(admin));
+    }
+
+
+    /**
+     * ログアウトする
+     * @return ログインページ
+     */
+    public Result logout() {
+        disconnect_session();
+        return redirect("/");
+    }
+
+
+    /**
+     * 管理者ページからidとpassword受け取って
+     * アカウント新規登録する
+     * @return 新規登録後のアカウントリスト
+     */
+    public Result signup() {
+        try {
+            Map<String, String[]> form = request().body().asFormUrlEncoded();
+            final String id = form.get("id")[0];
+            final String password = form.get("password")[0];
+            final String name = form.get("name")[0];
+            final Account.SexTag sex = Account.SexTag.valueOf(form.get("sex")[0]);
+            final String address = form.get("address")[0];
+            //生徒のidはS、教師のidはTから始まることを想定
+            if(id.startsWith("S")){
+                if(get_student(id) != null) return unauthorized();
+                final Student student = new Student(id, password, name, sex, address);
+                students.add(student);
+                return student_list();
+            } else if(id.startsWith("T")){
+                if(get_teacher(id) != null) return unauthorized();
+                final Teacher teacher = new Teacher(id, password, name, sex, address);
+                teachers.add(teacher);
+                return teacher_list();
+            } else {
+                return badRequest();
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            return badRequest();
+        }
+    }
+}
