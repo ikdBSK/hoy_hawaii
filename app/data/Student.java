c4f2467ba7f0ba96d6f4591b1309d4e5ad3f47ec
Tatsuki Ikeda <3p.ahirunosora@gmail.com>
2019-01-21 14:38:39 +0900

    模試関係2019/1/21

--- app/data/Student.java
+++ app/data/Student.java
@@ -4,6 +4,7 @@ import data.record.SchoolRecord;
 
 public class Student extends Account {
     private SchoolRecord record;
+    private ExternalRecord exRecord;
 
     public Student(String id, String password, String name, SexTag sex, String address) {
         super(id, password, name, sex, address);
@@ -12,5 +13,9 @@ public class Student extends Account {
 
     public SchoolRecord getRecord() {
         return record;
+    }
+
+    public ExternalRecord getExRecord() {
+        return exRecord;
     }
 }
