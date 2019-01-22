8b4b1278699fc3d28e8d3a9285ceb79d2023cf0c
HaneishiMasahiko <haneishi.m.aa@m.titech.ac.jp>
2019-01-22 12:19:52 +0900

    偏差値計算追加

--- app/data/record/SchoolExam.java
+++ app/data/record/SchoolExam.java
@@ -72,6 +72,40 @@ public class SchoolExam {
     }
 
     /**
+     * 指定された生徒の指定された科目の偏差値
+     * @param student テストを返却したい対象生徒
+     * @return 該当する生徒の偏差値、存在しないまたは未公開の場合は0
+     */
+    public double getDValue(Student student, Subject subject) {
+        if(release==false){
+            return 0;
+        }
+        for(SchoolTest test : tests){
+            if(test.getSubject().equals(subject)){
+                TestResult result = test.getResult().get(student);
+                ArrayList<TestResult> results = new ArrayList<>(test.getResult().values());
+                if(result != null){
+
+                    double sum = 0;
+                    for(TestResult t : results){
+                        sum += t.getScore();
+                    }
+                    double mean = sum / results.size();
+                    double ssum = 0.0;
+                    for(TestResult t : results){
+                        ssum += Math.pow(t.getScore()-mean, 2);
+                    }
+                    double variance = ssum / results.size();
+                    double sd = Math.sqrt(variance);
+
+                    return ((result.getScore() - mean) * 10 / sd) + 50;
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
      * 成績公開を行う
      */
     public void release(){
