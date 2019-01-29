3e4d6a5e9ad54a7446760b90cfe81a73f429c30a
ikdBSK <3p.ahirunosora@gmail.com>
2019-01-29 11:55:41 +0900

    2019/1/29 偏差値、順位フィールド追加

--- app/data/record/ExternalTestResult.java
+++ app/data/record/ExternalTestResult.java
@@ -8,12 +8,14 @@ public class ExternalTestResult {
 	private Student student;
 	private Subject subject;
 	private double dValue;
+	private int rank;
 
-	public ExternalTestResult(int score, Student student, Subject subject, double dValue){
+	public ExternalTestResult(int score, Student student, Subject subject, double dValue, int rank){
 		this.score = score;
 		this.student = student;
 		this.subject = subject;
 		this.dValue = dValue;
+		this.rank = rank;
 	}
 
 	public int getScore(){ return score; }
@@ -28,11 +30,11 @@ public class ExternalTestResult {
 
 	public void setSubject(Subject subject) { this.subject = subject; }
 
-	public double getDValue() {
-		return dValue;
-	}
+	public double getDValue() { return dValue; }
 
-	public void setDValue(double dValue) {
-		this.dValue = dValue;
-	}
+	public void setDValue(double dValue) { this.dValue = dValue; }
+
+	public int getRank() { return rank; }
+
+	public void setRank(int rank) { this.rank = rank; }
 }
